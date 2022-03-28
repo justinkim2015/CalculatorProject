07f96286c68713dd51771c04700bf216fae4f7a7
justinkim2015 <justinkim2015@gmail.com>
2022-03-28 16:36:31 +0900

    Refactor code using event delegation

--- main.js
+++ main.js
@@ -1,184 +1,81 @@
-const oneButton = document.querySelector('.one');
-const twoButton = document.querySelector('.two');
-const threeButton = document.querySelector('.three');
-const fourButton = document.querySelector('.four');
-const fiveButton = document.querySelector('.five');
-const sixButton = document.querySelector('.six');
-const sevenButton = document.querySelector('.seven');
-const eightButton = document.querySelector('.eight');
-const nineButton = document.querySelector('.nine');
-const zeroButton = document.querySelector('.zero');
-const clearButton = document.querySelector('.clear');
-const additionButton = document.querySelector('.plus');
-const minusButton = document.querySelector('.minus');
-const multiplyButton = document.querySelector('.multiply');
-const divideButton = document.querySelector('.divide');
-const equalsButton = document.querySelector('.equals');
+const allNumbers = document.querySelector('.numbers');
+const allOperators = document.querySelector('.operators');
 
 let displayNumber = 0;
 let firstNumber = 0;
+let secondNumber = 0; 
 let operation = "";
 let calculator = document.querySelector('.top');
 let display = document.querySelector('.screen');
 display.textContent = displayNumber;
 
 //eventlisteners
-equalsButton.addEventListener('click', () => {
-    displayNumber = operate(parseInt(firstNumber, 10), parseInt(displayNumber, 10), operation);
-    resetScreen();
-    console.log(firstNumber);
-    console.log(displayNumber);
-});
-
-additionButton.addEventListener('click', () => {
-    firstNumber = displayNumber;
-    displayNumber = 0;
-    operation = '+';
-    resetScreen();
-    console.log(firstNumber);
-    console.log(displayNumber);
-});
-
-minusButton.addEventListener('click', () => {
-    firstNumber = displayNumber;
-    displayNumber = 0;
-    operation = '-';
-    resetScreen();
-    console.log(firstNumber);
-    console.log(displayNumber);
-});
-
-multiplyButton.addEventListener('click', () => {
-    firstNumber = displayNumber;
-    displayNumber = 0;
-    operation = '*';
-    resetScreen();
-    console.log(firstNumber);
-    console.log(displayNumber);
-});
-
-divideButton.addEventListener('click', () => {
-    firstNumber = displayNumber;
-    displayNumber = 0;
-    operation = '/';
-    resetScreen();
-    console.log(firstNumber);
-    console.log(displayNumber);
-});
-
-clearButton.addEventListener('click', () => {
-    displayNumber = 0;
-    firstNumber = 0;
-    operation = "";
-    resetScreen ();
-});
-
-// *********************************IF ELSE */
-// oneButton.addEventListener('click', () => {
-//     if (displayNumber == 0) {
-//         displayNumber = displayNumber + 1;
-//     } else {
-//         displayNumber = displayNumber + '1';
-//     } 
-//     resetScreen();   
-// }); 
-
-oneButton.addEventListener('click', () => {
-    displayNumber = (displayNumber == 0) ? displayNumber + 1 : displayNumber + '1';
-    resetScreen();
-});
-//**************************TERNARY^^^^^^ */
-
-twoButton.addEventListener('click', () => {
-    if (displayNumber == 0) {
-        displayNumber = displayNumber + 2;
-        resetScreen ()
+//USE THE STORED OPERATION VALUE TO KNOW WHICH OPERATOR TO USE WHEN SWITCHING
+allOperators.addEventListener('click', () => {
+    if (event.target.textContent == '+') {
+        operation = '+';
+        firstNumber = displayNumber;
+        displayNumber = "";
+        resetScreen();
+    } else if (event.target.textContent == '-') {
+        operation = '-';
+        firstNumber = displayNumber;
+        displayNumber = "";
+        resetScreen();
+    } else if (event.target.textContent == '*') {
+        operation = '*';
+        firstNumber = displayNumber;
+        displayNumber = "";
+        resetScreen();
+    } else if (event.target.textContent == '/') {
+        operation = '/';
+        firstNumber = displayNumber;
+        displayNumber = "";
+        resetScreen();
+    } else if (event.target.textContent == '=') {
+        displayNumber = operate(parseInt(firstNumber, 10), parseInt(displayNumber, 10), operation);
+        resetScreen();
     } else {
-        displayNumber = displayNumber + '2';
-        resetScreen ()
-    }    
-}); 
-
-threeButton.addEventListener('click', () => {
-    if (displayNumber == 0) {
-        displayNumber = displayNumber + 3;
-        resetScreen ()
-    } else {
-        displayNumber = displayNumber + '3';
-        resetScreen ()
-    }    
-}); 
-
-fourButton.addEventListener('click', () => {
-    if (displayNumber == 0) {
-        displayNumber = displayNumber + 4;
-        resetScreen ()
-    } else {
-        displayNumber = displayNumber + '4';
-        resetScreen ()
-    }    
-}); 
-
-fiveButton.addEventListener('click', () => {
-    if (displayNumber == 0) {
-        displayNumber = displayNumber + 5;
-        resetScreen ()
-    } else {
-        displayNumber = displayNumber + '5';
-        resetScreen ()
-    }    
-}); 
-
-sixButton.addEventListener('click', () => {
-    if (displayNumber == 0) {
-        displayNumber = displayNumber + 6;
-        resetScreen ()
-    } else {
-        displayNumber = displayNumber + '6';
-        resetScreen ()
-    }    
-}); 
-
-sevenButton.addEventListener('click', () => {
-    if (displayNumber == 0) {
-        displayNumber = displayNumber + 7;
-        resetScreen ()
-    } else {
-        displayNumber = displayNumber + '7';
-        resetScreen ()
-    }    
-}); 
-
-eightButton.addEventListener('click', () => {
-    if (displayNumber == 0) {
-        displayNumber = displayNumber + 8;
-    } else {
-        displayNumber = displayNumber + '8';
+        displayNumber = 0;
+        firstNumber = 0;
+        operation = "";
+        resetScreen ();    
     }
-    resetScreen();    
-}); 
+})
 
-nineButton.addEventListener('click', function() {
-    if (displayNumber == 0) {
-        displayNumber = displayNumber + 9;
-        resetScreen ()
+allNumbers.addEventListener('click', () => {
+    if (event.target.textContent == '1') {
+        displayNumber = (displayNumber == 0) ? displayNumber + 1 : displayNumber + '1';
+        resetScreen();    
+    } else if (event.target.textContent == '2') {
+        displayNumber = (displayNumber == 0) ? displayNumber + 2 :displayNumber + '2';
+        resetScreen();
+    } else if (event.target.textContent == '3') {
+        displayNumber = (displayNumber == 0) ? displayNumber + 3 :displayNumber + '3';
+        resetScreen();
+    } else if (event.target.textContent == '4') {
+        displayNumber = (displayNumber == 0) ? displayNumber + 4 :displayNumber + '2';
+        resetScreen();
+    } else if (event.target.textContent == '5') {
+        displayNumber = (displayNumber == 0) ? displayNumber + 5 :displayNumber + '5';
+        resetScreen();
+    } else if (event.target.textContent == '6') {
+        displayNumber = (displayNumber == 0) ? displayNumber + 6 :displayNumber + '6';
+        resetScreen();
+    } else if (event.target.textContent == '7') {
+        displayNumber = (displayNumber == 0) ? displayNumber + 7 :displayNumber + '7';
+        resetScreen();
+    } else if (event.target.textContent == '8') {
+        displayNumber = (displayNumber == 0) ? displayNumber + 8 :displayNumber + '8';
+        resetScreen();
+    } else if (event.target.textContent == '9') {
+        displayNumber = (displayNumber == 0) ? displayNumber + 9 :displayNumber + '9';
+        resetScreen();
     } else {
-        displayNumber = displayNumber + '9';
-        resetScreen ()
-    }    
-    console.log(displayNumber)
-}); 
-
-zeroButton.addEventListener('click', function() {
-    if (displayNumber == 0) {
-        displayNumber = displayNumber + 0;
-        resetScreen ()
-    } else {
-        displayNumber = displayNumber + '0';
-        resetScreen ()
-    }    
-    console.log(displayNumber)
-}); 
+        displayNumber = (displayNumber == 0) ? displayNumber + 0 : displayNumber + '0'; 
+        resetScreen();
+    }
+})
 
 //screen functions
 function resetScreen () {
@@ -219,3 +116,8 @@ function divide (x, y) {
     return x / y;
 }
   
+// Each button contains some value, right? It can be a digit or operator key. Just check for it.
+// Or you can add one handler to number keys and another handler to operator keys.
+
+// also I feel like there's probably a more elegant way of adding EventListeners rather than just making an individual one for each button
+// Read about event delegation. 
